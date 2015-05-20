961fc887327c79b5a4e400c66b8025e3e84f4a39
Ozan Seymen <oseymen@gmail.com>
2015-05-20 12:02:40 +0100

    modified example project folder structure

--- examples/simple-proxy/gulpfile.js
+++ examples/simple-proxy/gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
 var zip = require('gulp-zip');
-var options = require('./config/test.js');
-var apigee = require('./source/index.js');
+var options = require('../../config/test.js');
+var apigee = require('../../source/index.js');
 
 gulp.task('apigee:import', function(){
-	gulp.src(['./examples/**'])
+	gulp.src(['./apiproxy/**'], {base: '.'})
 		.pipe(zip('apiproxy.zip'))
 		.pipe(apigee.import(options));
 });
